b9e3c97749ae27d13e42728fb5c81c22d15ddf80
Soma Holiday <soma@somaholiday.com>
2016-01-23 05:24:27 +0000

    Initial commit : Hall Effect Sensor working

---
+++ main.js
@@ -0,0 +1,45 @@
+var OPC = new require("./fadecandy/examples/node/opc")
+var client = new OPC("localhost", 7890);
+
+var Gpio = require("onoff").Gpio;
+var hallPin = new Gpio( 4, "in", "both" );
+
+var isOpen = false;
+
+hallPin.watch( function(err, value) {
+	if (err) throw err;
+	
+	isOpen = !!value;
+
+	console.log("isOpen : " + isOpen);
+});
+
+function exit() {
+	hallPin.unexport();
+}
+
+process.on("SIGINT", exit);
+
+
+function draw() {
+	if (isOpen) {
+		var millis = new Date().getTime();
+
+		for ( var pixel = 0; pixel < 64; pixel++ ) {
+			var t = pixel * 0.2 + millis * 0.002;
+			var red =   128 + 96 * Math.sin(t);
+			var green = 128 + 96 * Math.sin(t + 0.1);
+			var blue =  128 + 96 * Math.sin(t + 0.3);
+
+			client.setPixel( pixel, red, green, blue ); 
+		}
+	} else {
+		for ( var pixel = 0; pixel < 64; pixel++ ) {
+			client.setPixel( pixel, 0, 0, 0 );
+		}
+	}
+	client.writePixels();
+}
+
+setInterval(draw, 30);
+
