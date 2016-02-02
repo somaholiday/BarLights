d7895097e0fc2f3bd11882472c5ef218189ba9ca
Soma Holiday <soma@somaholiday.com>
2016-02-01 17:39:59 -0800

    Try addressing 3 strips

--- app/main.js
+++ app/main.js
@@ -2,55 +2,60 @@ var OPC = new require("./app/fadecandy/examples/node/opc")
 var client = new OPC("server", 7890);
 
 var Gpio = require("onoff").Gpio;
-var hallPin = new Gpio( 4, "in", "both" );
+var hallPin = new Gpio(4, "in", "both");
 
+var STRIP_COUNT = 3;
 var PIXEL_COUNT = 31;
 var FADE_TIME = 750; //ms
 
 var isOpen = false;
 var lastChange = 0;
 
-hallPin.watch( function(err, value) {
-	if (err) throw err;
-	
-	isOpen = !!value;
+hallPin.watch(function(err, value) {
+    if (err) throw err;
 
-	console.log("isOpen : " + isOpen);
+    isOpen = !!value;
 
-  lastChange = new Date().getTime();
+    console.log("isOpen : " + isOpen);
+
+    lastChange = new Date().getTime();
 });
 
 function exit() {
-	hallPin.unexport();
+    hallPin.unexport();
 }
 
 process.on("SIGINT", exit);
 
 
 function draw() {
-	if (isOpen) {
-    var millis = new Date().getTime();
-    var timeSinceChange = millis - lastChange;
+    if (isOpen) {
 
-    var fadeFactor = Math.min(1, timeSinceChange / FADE_TIME);
+        var millis = new Date().getTime();
+        var timeSinceChange = millis - lastChange;
 
-		for ( var pixel = 0; pixel < PIXEL_COUNT; pixel++ ) {
-			var t = pixel * 0.2 + millis * 0.002;
-			var red =   128 + 96 * Math.sin(t);
-			var green = 128 + 96 * Math.sin(t + 0.1);
-			var blue =  128 + 96 * Math.sin(t + 0.3);
+        var fadeFactor = Math.min(1, timeSinceChange / FADE_TIME);
+        for (var strip = 0; strip < STRIP_COUNT; strip++) {
+            for (var pixel = 0; pixel < PIXEL_COUNT; pixel++) {
+                var t = pixel * 0.2 + millis * 0.002;
+                var red = 128 + 96 * Math.sin(t);
+                var green = 128 + 96 * Math.sin(t + 0.1);
+                var blue = 128 + 96 * Math.sin(t + 0.3);
 
-			client.setPixel( pixel, fadeFactor * red, fadeFactor * green, fadeFactor * blue ); 
-		}
-	} else {
+                client.setPixel( (strip*64) + pixel, fadeFactor * red, fadeFactor * green, fadeFactor * blue);
+            }
+        }
 
+    } else {
 
-		for ( var pixel = 0; pixel < PIXEL_COUNT; pixel++ ) {
-			client.setPixel( pixel, 0, 0, 0 );
-		}
-	}
-	client.writePixels();
+        for (var strip = 0; strip < STRIP_COUNT; strip++) {
+            for (var pixel = 0; pixel < PIXEL_COUNT; pixel++) {
+                client.setPixel( (strip*64) + pixel, 0, 0, 0);
+            }
+        }
+
+    }
+    client.writePixels();
 }
 
 setInterval(draw, 30);
-
