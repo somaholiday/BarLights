9a837c195a7076a08de420b01fed232dc6bf7f36
Soma Holiday <soma@somaholiday.com>
2016-02-06 20:25:16 -0800

    Update simulator layout

--- app/main.js
+++ app/main.js
@@ -1,32 +1,12 @@
-var OPC = new require("./app/fadecandy/examples/node/opc")
-var client = new OPC("server", 7890);
-
-var Gpio = require("onoff").Gpio;
-var hallPin = new Gpio(4, "in", "both");
+var OPC = new require("../../../fadecandy/examples/node/opc")
+var client = new OPC("localhost", 7890);
 
 var STRIP_COUNT = 3;
 var PIXEL_COUNT = 31;
 var FADE_TIME = 750; //ms
 
-var isOpen = false;
+var isOpen = true;
 var lastChange = 0;
-
-hallPin.watch(function(err, value) {
-    if (err) throw err;
-
-    isOpen = !!value;
-
-    console.log("isOpen : " + isOpen);
-
-    lastChange = new Date().getTime();
-});
-
-function exit() {
-    hallPin.unexport();
-}
-
-process.on("SIGINT", exit);
-
 
 function draw() {
     if (isOpen) {
@@ -38,8 +18,8 @@ function draw() {
         for (var strip = 0; strip < STRIP_COUNT; strip++) {
             for (var pixel = 0; pixel < PIXEL_COUNT; pixel++) {
                 var t = pixel * 0.2 + millis * 0.002;
-                var red = 128 + 96 * Math.sin(t);
-                var green = 128 + 96 * Math.sin(t + 0.1);
+                var red = 0;//128 + 96 * Math.sin(t);
+                var green = 0;//128 + 96 * Math.sin(t + 0.1);
                 var blue = 128 + 96 * Math.sin(t + 0.3);
 
                 client.setPixel((strip * 64) + pixel, fadeFactor * red, fadeFactor * green, fadeFactor * blue);
--- layouts/barlights.py
+++ layouts/barlights.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
-spacing = 0.1
-row_height = 0.5
+spacing = 0.15
+row_height = 1.2
 lines = []
 for r in range(3):
+  pixel_count = 0
   for c in range(-14, 15):
     x, y, z = -c*spacing, r*row_height, 0
     lines.append('  {"point": [%.2f, %.2f, %.2f]}' % (x, y, z))
+    pixel_count = pixel_count + 1
+
+  for d in range(pixel_count, 64):
+    lines.append('  {"point": [%.2f, %.2f, %.2f]}' % (999, 999, 999))
+
 print '[\n' + ',\n'.join(lines) + '\n]'
